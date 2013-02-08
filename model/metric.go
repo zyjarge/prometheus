f28de6f3e3370e20b1eb0487abfb56643478a114
Matt T. Proud <matt.proud@gmail.com>
2013-02-08 18:03:26 +0100

    Checkpoint.

--- model/metric.go
+++ model/metric.go
@@ -18,7 +18,6 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"sort"
 	"time"
 )
 
@@ -26,17 +25,6 @@ const (
 	// XXX: Re-evaluate down the road.
 	reservedDelimiter = `"`
 )
-
-// A Fingerprint is a simplified representation of an entity---e.g., a hash of
-// an entire Metric.
-type Fingerprint string
-
-// A LabelName is a key for a LabelSet or Metric.  It has a value associated
-// therewith.
-type LabelName string
-
-// A LabelValue is an associated value for a LabelName.
-type LabelValue string
 
 // A LabelSet is a collection of LabelName and LabelValue pairs.  The LabelSet
 // may be fully-qualified down to the point where it may resolve to a single
@@ -96,42 +84,6 @@ func (l LabelNames) Swap(i, j int) {
 // a singleton and refers to one and only one stream of samples.
 type Metric map[LabelName]LabelValue
 
-type Fingerprints []Fingerprint
-
-func (f Fingerprints) Len() int {
-	return len(f)
-}
-
-func (f Fingerprints) Less(i, j int) bool {
-	return sort.StringsAreSorted([]string{string(f[i]), string(f[j])})
-}
-
-func (f Fingerprints) Swap(i, j int) {
-	f[i], f[j] = f[j], f[i]
-}
-
-// Fingerprint generates a fingerprint for this given Metric.
-func (m Metric) Fingerprint() Fingerprint {
-	labelLength := len(m)
-	labelNames := make([]string, 0, labelLength)
-
-	for labelName := range m {
-		labelNames = append(labelNames, string(labelName))
-	}
-
-	sort.Strings(labelNames)
-
-	summer := md5.New()
-
-	for _, labelName := range labelNames {
-		summer.Write([]byte(labelName))
-		summer.Write([]byte(reservedDelimiter))
-		summer.Write([]byte(m[LabelName(labelName)]))
-	}
-
-	return Fingerprint(hex.EncodeToString(summer.Sum(nil)))
-}
-
 // A SampleValue is a representation of a value for a given sample at a given
 // time.  It is presently float32 due to that being the representation that
 // Protocol Buffers provide of floats in Go.  This is a smell and should be
@@ -144,12 +96,6 @@ func (v SampleValue) MarshalJSON() ([]byte, error) {
 
 func (s SamplePair) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("{\"Value\": \"%f\", \"Timestamp\": %d}", s.Value, s.Timestamp.Unix())), nil
-}
-
-type Sample struct {
-	Metric    Metric
-	Value     SampleValue
-	Timestamp time.Time
 }
 
 type SamplePair struct {
@@ -179,11 +125,4 @@ type SampleSet struct {
 type Interval struct {
 	OldestInclusive time.Time
 	NewestInclusive time.Time
-}
-
-// PENDING DELETION BELOW THIS LINE
-
-type Samples struct {
-	Value     SampleValue
-	Timestamp time.Time
 }
